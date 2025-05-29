64db7a1793c6d5c49bf605b962e4a0264eaab3fe
hjgfnhinlfgihngfinhnilgfnijlhnjilgf <136336190+hjgfnhinlfgihngfinhnilgfnijlhnjilgf@users.noreply.github.com>
2025-05-30 00:03:18 +0200

    Add files via upload

---
+++ output/networksystem_dll.hpp
@@ -0,0 +1,21 @@
+// Dumped by amphesiaxyz using Atlas Dumper
+// 2025-05-29 11:19:28.655222400 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: networksystem.dll
+        // Class count: 1
+        // Enum count: 0
+        namespace networksystem_dll {
+            // Parent: None
+            // Field count: 1
+            namespace ChangeAccessorFieldPathIndex_t {
+                constexpr std::ptrdiff_t m_Value = 0x0; // int32
+            }
+        }
+    }
+}
